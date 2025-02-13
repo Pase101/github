f156e69b5d7101ea11860df9182bf9debeda390c
Pase101 <pascalteschke47@gmail.com>
2025-02-13 21:57:25 +0100

    Update list.lua

--- list.lua
+++ list.lua
@@ -1,9 +1,10 @@
 local list = {
-1825621146,
-2556084505, 
-4677863378,
---5370829337,
-1935561886
+1825621146, --pasql3
+2556084505, --pasql44
+4677863378, --DeadlySins_16
+5733662243,  --kitkat
+--5370829337, --pasql101
+1935561886 --bmoney7755
 }
 return table.find(list, game.Players.LocalPlayer.UserId) ~= nil
 
